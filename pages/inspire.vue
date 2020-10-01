da7c46f264744ec94efa2e3b8099e48db38ba282
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-10-01 10:08:25 -0300

    Block access for unauthenticated users

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -3,8 +3,34 @@
     <h2 class="title is-3 has-text-grey">
       "Just start <b-icon icon="rocket" size="is-large" />"
     </h2>
+    <p>Usuário logado: {{ login }}</p>
     <h3 class="subtitle is-6 has-text-grey">
-      Autor: <a href="https://github.com/ErickPetru">Erick Petrucelli</a>
+      Autor: <a :href="author.github">{{ author.name }}</a>
     </h3>
   </section>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      author: {
+        github: 'https://github.com/ErickPetru',
+        name: 'Erick Petrucelli',
+      },
+    }
+  },
+  computed: {
+    ...mapState({
+      login: (state) => state.user.login,
+    }),
+  },
+  beforeMount() {
+    if (!this.login) {
+      this.$router.push('/')
+    }
+  },
+}
+</script>
