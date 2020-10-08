ced65980d1d1b1bfb0a93a90c7fc9b669c65e763
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-10-08 09:47:34 -0300

    Atualizando página de testes

--- pages/teste.vue
+++ pages/teste.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>Olá, este é meu teste!</div>
+  <div>
+    <h2>Olá, isso é um teste!</h2>
+    <p>Passe o mouse para ver a imagem.</p>
+
+    <div
+      class="image-box"
+      :class="{ 'with-image': image }"
+      @mouseenter="showImage"
+      @mouseleave="hideImage"
+    ></div>
+
+    <ul>
+      <li v-for="day of weekDays" :key="day.id">{{ day.name }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      image: false,
+      weekDays: [
+        { id: 1, name: 'seg' },
+        { id: 2, name: 'ter' },
+        { id: 3, name: 'qua' },
+        { id: 4, name: 'qui' },
+        { id: 5, name: 'sex' },
+      ],
+    }
+  },
+  methods: {
+    showImage() {
+      this.image = true
+    },
+
+    hideImage() {
+      this.image = false
+    },
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 div {
-  color: red;
+  text-align: center;
+}
+
+.image-box {
+  width: 30rem;
+  height: 30rem;
+  border: 1px solid #ccc;
+  background: #ddd;
+  margin: auto;
+}
+
+.with-image {
+  background: url('https://media.giphy.com/media/fUYhyT9IjftxrxJXcE/giphy.gif');
+  background-size: cover;
 }
 </style>
